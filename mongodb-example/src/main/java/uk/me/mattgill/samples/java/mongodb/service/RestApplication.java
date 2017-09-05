11824929bb5a81596aef4dffeecabc024aee6fc9
Matt Gill <matthew.gill@live.co.uk>
2017-09-05 09:34:25 +0100

    Created mongodb test project

---
+++ mongodb-example/src/main/java/uk/me/mattgill/samples/java/mongodb/service/RestApplication.java
@@ -0,0 +1,9 @@
+package uk.me.mattgill.samples.java.mongodb.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+
+}
