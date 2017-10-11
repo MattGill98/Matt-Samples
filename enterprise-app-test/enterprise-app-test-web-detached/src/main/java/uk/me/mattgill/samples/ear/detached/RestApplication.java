0e7ce32c6131f8089526840c72783cd0e536a7e8
Matt Gill <matthew.gill@live.co.uk>
2017-10-11 09:34:16 +0100

    Removed enterprise app test

--- enterprise-app-test/enterprise-app-test-web-detached/src/main/java/uk/me/mattgill/samples/ear/detached/RestApplication.java
+++
@@ -1,9 +0,0 @@
-package uk.me.mattgill.samples.ear.detached;
-
-import javax.ws.rs.ApplicationPath;
-import javax.ws.rs.core.Application;
-
-@ApplicationPath("/test")
-public class RestApplication extends Application {
-
-}
