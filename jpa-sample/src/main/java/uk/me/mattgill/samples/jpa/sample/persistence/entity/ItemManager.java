f82ae7e32a2150d0c46c76f7dc410574705d2fc0
Matt Gill <matthew.gill@live.co.uk>
2017-10-13 15:21:06 +0100

    Added descriptions and names to each project

--- jpa-sample/src/main/java/uk/me/mattgill/samples/jpa/sample/persistence/entity/ItemManager.java
+++ jpa-sample/src/main/java/uk/me/mattgill/samples/jpa/sample/persistence/entity/ItemManager.java
@@ -3,7 +3,6 @@ package uk.me.mattgill.samples.jpa.sample.persistence.entity;
 import java.util.ArrayList;
 import java.util.List;
 import javax.enterprise.context.RequestScoped;
-import javax.persistence.EntityExistsException;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -13,18 +12,18 @@ public class ItemManager {
 
     @PersistenceContext(unitName = "itemPU")
     private EntityManager em;
-    
+
     @Transactional
     public List<Item> getAllItems() {
         return (List<Item>) em.createNamedQuery("item.getAll").getResultList();
     }
-    
+
     @Transactional
     public List<Item> clearItems() {
         em.createNamedQuery("item.clearAll").executeUpdate();
         return new ArrayList<>();
     }
-    
+
     @Transactional
     public Item insertItem(Item item) {
         Item existingItem = em.find(Item.class, item.getName());
@@ -37,5 +36,5 @@ public class ItemManager {
             return existingItem;
         }
     }
-    
+
 }
