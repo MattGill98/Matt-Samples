a5cd6af739e47d531e8df1b2a136f170d6419b43
Matt Gill <matthew.gill@live.co.uk>
2017-12-07 12:15:15 +0000

    Made logging service print every 2 seconds

--- logging-test/src/main/java/uk/me/mattgill/samples/log/test/PrintService.java
+++ logging-test/src/main/java/uk/me/mattgill/samples/log/test/PrintService.java
@@ -1,5 +1,8 @@
 package uk.me.mattgill.samples.log.test;
 
+import java.util.concurrent.TimeUnit;
+import javax.annotation.Resource;
+import javax.enterprise.concurrent.ManagedScheduledExecutorService;
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.context.Initialized;
 import javax.enterprise.event.Observes;
@@ -11,12 +14,19 @@ import uk.me.mattgill.samples.log.test.beans.PrintingBean;
 public class PrintService {
 
     @Inject
-    Instance<PrintingBean> printingBeans;
+    private Instance<PrintingBean> printingBeans;
 
+    @Resource
+    private ManagedScheduledExecutorService executor;
+
+    /**
+     * On startup, start calling the print method on all registered beans every
+     * 2 seconds.
+     *
+     * @param init the context object creating the bean.
+     */
     public void startup(@Observes @Initialized(ApplicationScoped.class) Object init) {
-        for (PrintingBean bean : printingBeans) {
-            bean.printToLog();
-        }
+        executor.scheduleAtFixedRate(() -> printingBeans.forEach(bean -> bean.printToLog()), 2, 2, TimeUnit.SECONDS);
     }
 
 }
