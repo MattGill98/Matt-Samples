4dae230d9ed30d4ccd87a5217d5f608dfb885f47
Matt Gill <matthew.gill@live.co.uk>
2017-12-08 13:50:23 +0000

    Updated hello-world resource to base context root.

--- jaxrs-hello-world/src/main/java/uk/me/mattgill/samples/jaxrs/hello/world/service/resource/HelloWorldResource.java
+++ jaxrs-hello-world/src/main/java/uk/me/mattgill/samples/jaxrs/hello/world/service/resource/HelloWorldResource.java
@@ -6,7 +6,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import uk.me.mattgill.samples.jaxrs.hello.world.api.TracedEndpoint;
 
-@Path("/hello")
+@Path("/")
 public class HelloWorldResource {
 
     @GET
