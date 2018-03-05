8ed3db8059c3da8b9afc6f1089c095bc954b6a6e
Matt Gill <matthew.gill@live.co.uk>
2018-03-05 10:50:36 +0000

    Fixed wrong character encoding.

--- grizzly-sample/src/main/java/uk/me/mattgill/samples/grizzly/FunctionalHttpHandler.java
+++ grizzly-sample/src/main/java/uk/me/mattgill/samples/grizzly/FunctionalHttpHandler.java
@@ -1,5 +1,7 @@
 package uk.me.mattgill.samples.grizzly;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import org.glassfish.grizzly.http.server.HttpHandler;
 import org.glassfish.grizzly.http.server.Request;
 import org.glassfish.grizzly.http.server.Response;
@@ -13,8 +15,8 @@ interface FunctionalHttpHandler {
         return new HttpHandler() {
             @Override
             public void service(Request request, Response response) throws Exception {
+                response.setCharacterEncoding(UTF_8.name());
                 doService(request, response);
-                response.setCharacterEncoding("utf-8");
             }
         };
     }
