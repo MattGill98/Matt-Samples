4e57f276bec330753b6feafe4945f02f7684d4d8
Matt Gill <matthew.gill@live.co.uk>
2018-06-12 16:29:42 +0100

    Added an OpenAPI sample.

---
+++ openapi-sample/src/main/java/uk/me/mattgill/samples/openapi/TestApplication.java
@@ -0,0 +1,9 @@
+package uk.me.mattgill.samples.openapi;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class TestApplication extends Application {
+
+}
