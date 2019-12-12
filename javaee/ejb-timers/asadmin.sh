c4117303e51cc6e3c95bcb84e6f737ed7b5ffa53
Matt Gill <matthew.gill@live.co.uk>
2019-12-12 10:48:41 +0000

    Configure server-config ejb timer source
    This makes the test work properly

--- javaee/ejb-timers/asadmin.sh
+++ javaee/ejb-timers/asadmin.sh
@@ -6,6 +6,7 @@ asadmin start-domain
 # Create instance config
 asadmin copy-config default-config test-config
 asadmin set configs.config.test-config.ejb-container.ejb-timer-service.ejb-timer-service=DataGrid
+asadmin set configs.config.server-config.ejb-container.ejb-timer-service.ejb-timer-service=DataGrid
 
 # Create deployment group and instances
 asadmin create-instance --node localhost-domain1 --config test-config instance1
